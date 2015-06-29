89a624c4b8e9398be32f908083ac54a4fca7807e
JD <jdstep23@gmail.com>
2015-06-28 20:47:31 -0700

    grunt runs concat and uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,15 +14,30 @@ module.exports = function(grunt) {
       '!client/services/googleanalytics.js'
       ]
     },
+    concat: {
+      options: {
+        seperator: ';'
+      },
+      dist: {
+        src: ['client/**/*.js'],
+        dest: 'public/js/sky.js'
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'public/js/sky.min.js': ['public/js/sky.js']
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 
 };
